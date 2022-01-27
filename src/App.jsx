357d9dead87baeb417999957fa2b09761290ac24
achieuw <achieuw@hotmail.com>
2022-01-27 16:13:03 +0100

    FEAT:Start view WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
-import Rasmus from './Components/Rasmus.jsx'
+import LoginView from './Views/LoginView.jsx'
+
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
-      <h1 className="m-2">Hello, Edwin!</h1>
-      <NavLink className="rounded-lg border-2 p-1 bg-pink-200" to="/rasmus">Go to Rasmus</NavLink>
-      <Routes >
-        <Route path="rasmus" element={<Rasmus />} />
-      </Routes>
+      <LoginView />
     </div>
-    </BrowserRouter>
   )
 }
 
