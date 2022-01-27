c2dad53523a4fee8908a691bd30210355fd189f8
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-27 16:37:18 +0100

    feat: add navbar text

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1 className="m-2">Hello, Edwin!</h1>
-      <NavLink className="rounded-lg border-2 p-1 bg-pink-200" to="/rasmus">Go to Rasmus</NavLink>
-      <Routes >
-        <Route path="rasmus" element={<Rasmus />} />
-      </Routes>
       <LoginView />
     </div>
   )
--- src/Views/NavBar.jsx
+++ src/Views/NavBar.jsx
@@ -2,21 +2,32 @@
  * Dependencies
  * @ignore
  */
-import Splash from '../../public/LostInTranslation_Resources/Splash.svg'
-import Logo from '../../public/LostInTranslation_Resources/Logo.png'
+import Splash from '/LostInTranslation_Resources/Splash.svg'
+import Logo from '/LostInTranslation_Resources/Logo.png'
+import ProfileHead from '../Components/NavBar/ProfileHead'
 
 
 /**
- * 
+ * Component
  * @ignore
  */
 const NavBar = () => {
   return (
-    <div className='flex bg-yellow-300'>
-        <img src={Splash} className='w-12'/>
-        <img src={Logo} className='absolute w-12'/>
-    </div>
+      <>
+        <div className='flex yellow border-color gap-4 py-2'>
+            <div className='flex relative w-12 ml-6'>
+                <img src={Splash} className=''/>
+                <img src={Logo} className='absolute w-12'/>
+            </div>
+            <h2 className='primary-font primary-text-col p-2'>Lost in Translation</h2>
+            <ProfileHead name="Edwin"/>
+        </div>
+      </>
   );
 };
 
+/**
+ * Exports
+ * @ignore
+ */
 export default NavBar
