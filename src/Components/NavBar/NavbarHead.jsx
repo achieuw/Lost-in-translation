fd4dcb667ecf8d10bb35fabfdabda432076dc7bc
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-28 08:34:28 +0100

    STYLE(navbarhead): add hover effect

--- src/Components/NavBar/NavbarHead.jsx
+++ src/Components/NavBar/NavbarHead.jsx
@@ -16,11 +16,11 @@ const ProfileHead = (props) => {
         <>
             {/* dont render if no name provided */}
             {name !== "" ? 
-            <div className="flex items-center">
+            <div className="flex items-center rounded-full w-fit hover:bg-slate-400 p-1">
                 <div className="rounded-l-full input-text-col bg-slate-300 items-center px-6 py-1 h-fit">
-                    <h5 className="primary-font primary-text-col opacity-100">{name}</h5> 
+                    <h5 className="primary-font primary-text-col opacity-100 pointer-events-none">{name}</h5> 
                 </div>
-                <div className='-m-2 w-12 bg-slate-300 rounded-full '>
+                <div className='-ml-2 w-12 bg-slate-300 rounded-full '>
                     <img className="invert" src={user} alt="" />
                 </div>
             </div>
