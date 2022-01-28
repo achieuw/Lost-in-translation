9a9b9a940ce57a5ee9a3a86e4e90e4d0819e944a
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-28 12:31:35 +0100

    STYLE(navbarhead): group hover

--- src/Components/NavBar/NavbarHead.jsx
+++ src/Components/NavBar/NavbarHead.jsx
@@ -16,11 +16,11 @@ const ProfileHead = (props) => {
         <>
             {/* dont render if no name provided */}
             {name !== "" ? 
-            <div className="flex items-center rounded-full w-fit hover:bg-slate-400 p-1">
-                <div className="rounded-l-full input-text-col bg-slate-300 items-center px-6 py-1 h-fit">
+            <div className="flex items-center w-fit group">
+                <div className="rounded-l-full input-text-col bg-slate-300 items-center px-6 py-1 h-fit group-hover:bg-slate-400">
                     <h5 className="primary-font primary-text-col opacity-100 pointer-events-none">{name}</h5> 
                 </div>
-                <div className='-ml-2 w-12 bg-slate-300 rounded-full '>
+                <div className='-ml-2 w-12 bg-slate-300 rounded-full group-hover:bg-slate-400'>
                     <img className="invert" src={user} alt="" />
                 </div>
             </div>
