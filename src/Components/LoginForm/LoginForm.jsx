cebc43b7dd43577aa23049b9db481efb18488c33
achieuw <achieuw@hotmail.com>
2022-01-29 18:37:19 +0100

    FEAT: invalid name error box

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,8 +1,9 @@
 import { useForm } from "react-hook-form";
+import { apiGetUser } from "../../Api/TranslationAPI";
 
 const usernameConfig = {
   required: true,
-  minLength: 3,
+  minLength: 2,
 };
 
 const LoginForm = () => {
@@ -12,32 +13,44 @@ const LoginForm = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    // Fetch user if it exists
-    // else submit user
-    console.log(data);
+  const onSubmit = async (data) => {
+    //console.log(data);
   };
+
+  const errorMessage = (() => {
+    if (!errors.username) {
+      return null;
+    }
+
+    if (errors.username.type === "required") {
+      return <span>Enter a name to continue</span>;
+    }
+
+    if (errors.username.type === "minLength") {
+      return <span>Name is too short (min. length 3)</span>;
+    }
+  })();
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="min-w-[360px] max-w-[500px] -mt-20 pb-12 pt-7 rounded-xl form-shadow z-30 linear-white-purple">
+      className="min-w-[360px] max-w-[500px] -mt-20 pb-12 pt-7 rounded-xl form-shadow z-30 linear-white-purple"
+    >
       <fieldset className="flex justify-center px-8">
         <div className="flex justify-center items-center h-14 g-gray-200 border-color-gray bg-gray-200 rounded-3xl">
-        <span className="material-icons px-2">edit</span>
-        <input
-          className="border-none rounded-3xl border-2 border-gray-400 input-text-col focus:text-gray-600 outline-none bg-transparent body-font text-2xl"
-          type="text"
-          placeholder="What's your name?"
-          {...register("username", usernameConfig)}
-        ></input>
-        <button
-          type="submit"
-          className="material-icons md-purple md-48">
-          arrow_circle_right
-        </button>
+          <span className="material-icons px-2">edit</span>
+          <input
+            className="border-none rounded-3xl border-2 border-gray-400 input-text-col focus:text-gray-600 outline-none bg-transparent body-font text-2xl"
+            type="text"
+            placeholder="What's your name?"
+            {...register("username", usernameConfig)}
+          ></input>
+          <button type="submit" className="material-icons md-purple md-48">
+            arrow_circle_right
+          </button>
         </div>
       </fieldset>
+      <div className="error-message rounded-lg mt-2 w-4/6 text-center mx-auto bg-red-600 text-red-50 body-font leading-10">{errorMessage}</div>
     </form>
   );
 };
