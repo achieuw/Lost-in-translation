9a2e798d09498553de3fcec76444c2ca01173e92
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-30 16:10:20 +0100

    FEAT: put info & logout in view

--- src/Views/ProfileView.jsx
+++ src/Views/ProfileView.jsx
@@ -1,7 +1,15 @@
+import ProfileInfo from '../Components/Profile/ProfileInfo';
+import ProfileLogout from '../Components/Profile/ProfileLogout';
+
 const ProfileView = () => {
+
+
   return (
     <>
-      <h1>Profile View</h1>
+      <div className="flex justify-between items-center primary-text-col w-full h-[40vh] yellow px-32">
+        <ProfileInfo />
+        <ProfileLogout />
+      </div>
     </>
   );
 };
