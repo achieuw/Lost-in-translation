b8ae351f6436c5f1a1ad188e10667ce85afa185b
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-30 16:22:52 +0100

    FEAT: add translations list to profile

--- src/Components/Profile/ProfileTranslationsList.jsx
+++ src/Components/Profile/ProfileTranslationsList.jsx
@@ -6,9 +6,10 @@ const ProfileTranslationsList = () => {
     const translations = [...user.translations, "hej", "test"]
 
     return (
-        <>
+        <div className="min-w-[360px] max-w-[500px] mt-10 pb-12 pt-7 rounded-xl form-shadow linear-white-purple">
+            <h1 className="primary-font">Translations</h1>
             { translations }
-        </>
+        </div>
     )
 };
 
--- src/Views/ProfileView.jsx
+++ src/Views/ProfileView.jsx
@@ -1,16 +1,18 @@
 import ProfileInfo from '../Components/Profile/ProfileInfo';
 import ProfileLogout from '../Components/Profile/ProfileLogout';
+import ProfileTranslationsList from '../Components/Profile/ProfileTranslationsList'
 
 const ProfileView = () => {
 
 
   return (
-    <>
+    <div className="flex flex-col items-center">
       <div className="flex justify-between items-center primary-text-col w-full h-[40vh] yellow px-32">
         <ProfileInfo />
         <ProfileLogout />
       </div>
-    </>
+      <ProfileTranslationsList />
+    </div>
   );
 };
  export default ProfileView
