ed7e8fb6fdf5e7b8c914062ed42ce9248222db34
achieuw <achieuw@hotmail.com>
2022-01-30 19:18:46 +0100

    FIX: Route to translate if user is logged in

--- src/Views/LoginView.jsx
+++ src/Views/LoginView.jsx
@@ -1,7 +1,19 @@
 import LoginForm from "../Components/LoginForm/LoginForm.jsx";
 import Header from "../Components/LoginForm/LoginHeader.jsx"
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useUserContext } from "../contexts/UserContext.jsx";
 
 const LoginView = () => {
+  const navigation = useNavigate()
+  const { user } = useUserContext()
+
+  useEffect(() => {
+    if(user.username !== "") {
+      navigation("./translation")
+    }
+  })
+
   return (
     // IMPORT NAVBAR COMPONENT
     <>
