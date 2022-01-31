26e63f9574c6a6067ca02d1f6d4975aa06059521
edwineliasson98 <edwin.eliasson.git@gmail.com>
2022-01-31 09:50:47 +0100

    FIX: remove single deletion

--- src/Components/Profile/ProfileTranslationItem.jsx
+++ src/Components/Profile/ProfileTranslationItem.jsx
@@ -3,21 +3,12 @@ import { useState } from "react";
 
 const ProfileTranslationItem = ({ translation }) => {
 
-    const [deleted, setDeleted] = useState(false)
-
-    const handleDeleteOnClick = () => {
-        setDeleted(true)
-    }
 
     return (
         <>
-            {!deleted ? 
-            <div className="group flex relative justify-center rounded-full border m-1 p-1 text-center body-font">
+            <div className="flex relative justify-center rounded-full border m-1 p-1 text-center body-font">
                 <p> { translation } </p>
-                <button onClick={ handleDeleteOnClick } className="absolute top-0 bottom-0 right-0 text-sm bg-red-600 hidden group-hover:block px-2 py-1 rounded-full text-white">Delete</button>
             </div>
-            : 
-            <></>}
         </>
     )
 };
