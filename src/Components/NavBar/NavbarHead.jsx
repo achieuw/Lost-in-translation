68c0ab04215e6d89d4532cd255f6641d13a2fe86
edwineliasson98 <edwin.eliasson.git@gmail.com>
2022-01-31 10:19:52 +0100

    STYLE: change background color of navbarhead

--- src/Components/NavBar/NavbarHead.jsx
+++ src/Components/NavBar/NavbarHead.jsx
@@ -17,10 +17,10 @@ const ProfileHead = (props) => {
             {/* dont render if no name provided */}
             {name !== "" ? 
             <div className="flex items-center w-fit group">
-                <div className="rounded-l-full input-text-col bg-slate-300 items-center px-6 py-1 h-fit group-hover:bg-slate-400 group-hover:cursor-pointer">
+                <div className="rounded-l-full input-text-col bg-gray-400 items-center px-6 py-1 h-fit group-hover:bg-gray-500 group-hover:cursor-pointer">
                     <h5 className="primary-font primary-text-col opacity-100 pointer-events-none">{name}</h5> 
                 </div>
-                <div className='-ml-2 w-12 bg-slate-300 rounded-full group-hover:bg-slate-400 group-hover:cursor-pointer'>
+                <div className='-ml-2 w-12 bg-gray-400 rounded-full group-hover:bg-gray-500 group-hover:cursor-pointer'>
                     <img className="invert" src={user} alt="" />
                 </div>
             </div>
