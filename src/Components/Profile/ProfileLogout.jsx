4990b3b695ae579d07c436723e8e5e878cf01929
edwineliasson98 <edwin.eliasson.git@gmail.com>
2022-01-31 13:58:39 +0100

    STYLE: add hover effect on profile buttons

--- src/Components/Profile/ProfileLogout.jsx
+++ src/Components/Profile/ProfileLogout.jsx
@@ -5,7 +5,7 @@ const ProfileLogout = ({ onLogoutClick }) => {
         onLogoutClick()
     }
 
-    return <button onClick={ handleOnLogoutClick } className="bg-red-600 rounded-full h-fit px-3 py-2 body-font">Log Out</button>;
+    return <button onClick={ handleOnLogoutClick } className="bg-red-600 rounded-full h-fit px-3 py-2 body-font hover:bg-red-700">Log Out</button>;
 };
 
 export default ProfileLogout
--- src/Components/Profile/ProfileTranslationsList.jsx
+++ src/Components/Profile/ProfileTranslationsList.jsx
@@ -12,7 +12,7 @@ const ProfileTranslationsList = ({ translations, handleDeleteTranslations }) => 
         <div className="flex flex-col min-w-[45%] max-w-[60%] mt-10 pb-12 pt-7 px-4 rounded-xl form-shadow linear-white-purple">
             <h1 className="primary-font">Translations</h1>
             { translationItems }
-            <button onClick={ handleDeleteLastTen } className="self-end text-sm bg-red-600 px-2 py-1 rounded-full text-white mx-1 mt-1 body-font">Delete translations</button>
+            <button onClick={ handleDeleteLastTen } className="self-end text-sm bg-red-600 px-2 py-1 rounded-full text-white mx-1 mt-1 body-font hover:bg-red-700">Delete translations</button>
         </div>
     )
 };
