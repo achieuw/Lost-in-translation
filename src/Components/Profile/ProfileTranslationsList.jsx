c1dd9f50baa7eefe026a7baf30e39b3b67668ea9
edwineliasson98 <edwin.eliasson.git@gmail.com>
2022-01-31 16:53:03 +0100

    STYLE: add text-center

--- src/Components/Profile/ProfileTranslationsList.jsx
+++ src/Components/Profile/ProfileTranslationsList.jsx
@@ -20,7 +20,7 @@ const ProfileTranslationsList = ({ translations, handleDeleteTranslations }) => 
 
     return (
         <div className="flex flex-col min-w-[45%] max-w-[60%] mt-10 pb-12 pt-7 px-4 rounded-xl form-shadow linear-white-purple">
-            <h1 className="primary-font">Translations</h1>
+            <h1 className="primary-font text-center">Translations</h1>
             { translationItems }
             <button onClick={ handleDeleteLastTen } className="self-end text-sm bg-red-600 px-2 py-1 rounded-full text-white mx-1 mt-1 body-font hover:bg-red-700">Delete translations</button>
         </div>
