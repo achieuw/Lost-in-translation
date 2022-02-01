d22b8dd2a0a94eeac3f92b63a1eec20bc92454ba
RasmusMoller <rasmus.moller@se.experis.com>
2022-02-01 09:32:06 +0100

    FIX: profile translation item overflow

--- src/Components/Profile/ProfileTranslationItem.jsx
+++ src/Components/Profile/ProfileTranslationItem.jsx
@@ -6,7 +6,7 @@ const ProfileTranslationItem = ({ translation }) => {
 
     return (
         <>
-            <div className="flex relative justify-center rounded-full border m-1 p-1 text-center body-font">
+            <div className="flex overflow-hidden relative justify-center rounded-full border m-1 p-1 text-center body-font">
                 <p> { translation } </p>
             </div>
         </>
