62ab1693e1b4c6d3f01d75ffdd90319c24e91a47
RasmusMoller <rasmus.moller@se.experis.com>
2022-02-01 09:00:12 +0100

    FEAT: Prettier 404

--- src/Views/NotFoundView.jsx
+++ src/Views/NotFoundView.jsx
@@ -1,8 +1,11 @@
+import HelloLogo from '/LostInTranslation_Resources/Logo-Hello-mad.png'
+
 const NotFoundView = () => {
   return (
-    <>
-      <h1>Page Not Found</h1>
-    </>
+    <div className="w-full">
+      <h1 className="text-center">404: Page Not Found</h1>
+      <img src={HelloLogo} className="mx-auto mt-6 w-72"/>
+    </div>
   );
 };
  export default NotFoundView
